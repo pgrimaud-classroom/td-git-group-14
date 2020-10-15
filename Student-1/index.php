82334829d554adc85176f8d8b7f3625d55f7c1bb
Karamoko Coulibaly <karamoko.coulibaly78200@gmail.com>
2020-10-15 15:15:42 +0200

    algo 3

--- Student-1/index.php
+++ Student-1/index.php
@@ -1,13 +1,31 @@
 <?php 
-//Algo 1
+echo 'Algo 1';
+echo'<br>';
 $x = 10;
- $y = 20;
+$y = 20;
+echo $x + $y;
+echo'<br>';
+echo'<br>';
 
- echo $x + $y;
-
- echo "Algo 2";
-
+echo "Algo 2";
+ echo'<br>';
  $x = 10;
-
  echo $x*2;
- 
+ echo'<br>';
+ echo'<br>';
+
+ echo 'Algo 3';
+ echo'<br>';
+ $x = rand (1 ,5);
+ if($x === 1){
+     echo 'Hello'; 
+ }
+ elseif($x === 2){
+     echo'Salut';
+ }
+ else{
+     echo 'Yo';
+ }
+
+ echo'<br>';
+ echo'<br>';
